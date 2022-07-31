0356b928a71f9d3b04e5074dc1832889d02e10ad
Kewal Suthar <102758565+Kewal170@users.noreply.github.com>
2022-07-31 21:33:57 +0530

    session feature added + download speed function added

--- kewal_youtube_downloader.py
+++ kewal_youtube_downloader.py
@@ -1,14 +1,23 @@
 import os
 import sys
+import speedtest
 
+from datetime import datetime
 from pytube import Playlist
 from pytube import YouTube
 
-path_video_file = 'Videos_from_youtube'                                        # Make sure you have change your location
+path_video_file = 'Videos_from_youtube'  # Make sure you have change your location
 path_playlist_file = 'Playlist_from_youtube'
+now = datetime.now()
 
 
-def video_info(link):                                  # This function will show information about that particular video
+def speed_testing():                # This function will give us our downloading speed [not used in this program]
+    st = speedtest.Speedtest()
+    download_result = st.download()
+    print(f"Download speed: {download_result / 1024 / 1024:.2f}Mb/s")
+
+
+def video_info(link):  # This function will show information about that particular video
     print("-" * 50)
     print("Title : ", link.title)
     print("Total Views : ", link.views, "k")
@@ -17,7 +26,7 @@ def video_info(link):                                  # This function will show
     print("-" * 50)
 
 
-def playlist_info(link):   # This function will show total number of videos present in the playlist along with the title
+def playlist_info(link):  # This function will show total number of videos present in the playlist along with the title
     try:
         print("-" * 50)
         count = 1
@@ -29,33 +38,37 @@ def playlist_info(link):   # This function will show total number of videos pres
         print(f"Error - {e}")
 
 
-def download_video(link):                                                   # This function will Download a single video
+def download_video(link):  # This function will Download a single video
     try:
+        print(f"> Session started : {now.strftime('%H:%M:%S')}")
         print("-" * 50)
         print(f"Downloading :>  {link.title}")
         link.streams.get_highest_resolution().download(path_video_file)
         print("-" * 50)
+        print(f"> Session ended : {now.strftime('%H:%M:%S')}")
         print("Download complete!")
         print(f"Output video location : /{path_video_file}/")
     except Exception as e:
         print(f"Error - {e}")
 
 
-def download_playlist(link):   # This function will Download the whole playlist and store it in a channel's named folder
+def download_playlist(link):  # This function will Download the whole playlist and store it in a channel's named folder
     os.mkdir(f"{path_playlist_file}/{link.owner}")
     try:
+        print(f"> Session started : {now.strftime('%H:%M:%S')}")
         print("-" * 50)
         for video in link.videos:
             print(f"Downloading :> {video.title}")
             video.streams.get_highest_resolution().download(f"{path_playlist_file}/{link.owner}")
         print("-" * 50)
+        print(f"> Session ended : {now.strftime('%H:%M:%S')}")
         print("Download Complete!")
         print(f"Output video location : /{path_playlist_file}/{link.owner}/")
     except Exception as e:
         print(f"Error - {e}")
 
 
-def help_menu():                                                                                             # Help menu
+def help_menu():  # Help menu
     print("-" * 50)
     print('''
         Syntax:-
@@ -72,7 +85,7 @@ def help_menu():                                                                
     print("-" * 50)
 
 
-if len(sys.argv) == 3:                                                                                    # Main program
+if len(sys.argv) == 3:  # Main program
     command = sys.argv[1]
     youtube_link = sys.argv[2]
     if command == 'info' and youtube_link != "":
